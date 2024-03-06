2d4fbf366f63f933a574362da6b2f8887a1219b2
wildhanhidayatullah <18221024@std.stei.itb.ac.id>
2024-03-07 00:56:13 +0700

    update : test to create splash and icons

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 1500);
 
 export default function App() {
-  return (
+  return (      
     <View style={styles.container}>
-      <Text>Let's get started!</Text>
+      <View style={styles.paragraph}>
+        <Text>GaLang merupakan aplikasi yang menghubungkan</Text>
+        <Text>para penyedia jasa sewa perlengkapan outdoor</Text>
+        <Text>dengan orang-orang yang membutuhkannya. GaLang</Text>
+        <Text>sendiri merupakan singkatan dari Garasi Petualang.</Text>
+        <Text></Text>
+        <Text>GaLang dikembangkan oleh:</Text>
+        <Text>Richard Haris 18221006 [Hustler]</Text>
+        <Text>Gyan Maiziko 18221008 [Hipster]</Text>
+        <Text>Wildhan Hidayatullah 18221024 [Hacker]</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E4E7C9',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  paragraph: {
+    color: '#000000',
+    fontSize: 18,
+    fontFamily: 'monospace',
+    fontWeight: 'bold',
+    textAlign: 'justify',
+  }
 });
